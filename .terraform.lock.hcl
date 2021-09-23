b66a60dc7045eed66f37222f950d7b11a8156953
Luiz Lelis <luizhlelis@gmail.com>
2021-09-23 19:54:14 -0300

    chore: creating an app registration with terraform

---
+++ .terraform.lock.hcl
@@ -0,0 +1,39 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/azuread" {
+  version     = "2.0.1"
+  constraints = "~> 2.0.0"
+  hashes = [
+    "h1:C9iv0cjLBaYJH/6c8hHu1RkuYUJU+FVad2fFaZC9Os4=",
+    "zh:045a5f5450eaf707b1a2c05188fa914f0364420da9d58c0bd29abc2985b838a8",
+    "zh:0aaf1bf334a55953289f0aead9cfaa2aaf37ffa7c4b6ca229d14918f18d006f9",
+    "zh:17914f4b85a18337e8e5acd644bd10c3e4309138983a9809e7a72bcc767835ff",
+    "zh:194dca0bdb636ce4289f5ff015623535898bdac26403a03649645cd4194b54ee",
+    "zh:53e1491141f23de8400092b5e17bf6e3ee8096dae6b58af2f8ca580337e31898",
+    "zh:656c729307e41f2f98bf5db634b74929d372189ff6dcb00e7d3370a7a36f3bd6",
+    "zh:7b8689cf5c4440ff0e21db9f9633478d50d220514fa9cd413ac2b7c93c7038e2",
+    "zh:cecd2259c04797297868236055d0f5808794677c43fe0c1e11d30f89f539b362",
+    "zh:da2a93d14318d9cace06aad77a0069ee0a0adb9e8337dac85434953db74dfa04",
+    "zh:eb127f8d11fc24f7070c14b6665f08bafd30e4038c1e4f5582a155f53ec795f6",
+    "zh:f99c245c80c5c4c418ef6f7b219482e56c714db65f970083bb287104a333e405",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/time" {
+  version = "0.7.2"
+  hashes = [
+    "h1:NKy1QrNLlP5mKy5Tea6lQSRsVoyydJQKh6WvNTdBF4I=",
+    "zh:0bbe0158c2a9e3f5be911b7e94477586110c51746bb13d102054f22754565bda",
+    "zh:3250af7fd49b8aaf2ccc895588af05197d886e38b727e3ba33bcbb8cc96ad34d",
+    "zh:35e4de0437f4fa9c1ad69aaf8136413be2369ea607d78e04bb68dc66a6a520b8",
+    "zh:369756417a6272e79cad31eb2c82c202f6a4b6e4204a893f656644ba9e149fa2",
+    "zh:390370f1179d89b33c3a0731691e772d5450a7d59fc66671ec625e201db74aa2",
+    "zh:3d12ac905259d225c685bc42e5507ed0fbdaa5a09c30dce7c1932d908df857f7",
+    "zh:75f63e5e1c68e6c5bccba4568c3564e2774eb3a7a19189eb8e2b6e0d58c8f8cc",
+    "zh:7c22a2078a608e3e0278c4cbc9c483909062ebd1843bddaf8f176346c6d378b1",
+    "zh:7cfb3c02f78f0060d59c757c4726ab45a962ce4a9cf4833beca704a1020785bd",
+    "zh:a0325917f47c28a2ed088dedcea0d9520d91b264e63cc667fe4336ac993c0c11",
+    "zh:c181551d4c0a40b52e236f1755cc340aeca0fb5dcfd08b3b1c393a7667d2f327",
+  ]
+}
---
+++ main.tf
@@ -0,0 +1,74 @@
+# Configure Terraform
+terraform {
+  required_providers {
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.0.1"
+    }
+  }
+}
+
+# Configure the Azure Active Directory as the Provider
+provider "azuread" { }
+
+# Retrieve domain information
+data "azuread_domains" "django_azure_b2c_auth" {
+  only_initial = true
+}
+
+# Client application section
+data "azuread_client_config" "current" {}
+
+resource "azuread_application" "django_azure_b2c_auth" {
+  display_name     = "django-azure-b2c-auth"
+
+  api {
+    mapped_claims_enabled          = true
+    requested_access_token_version = 2
+
+    oauth2_permission_scope {
+      admin_consent_description  = "Allow the application to access django-azure-b2c-auth on behalf of the signed-in user."
+      admin_consent_display_name = "Access django-azure-b2c-auth"
+      enabled                    = true
+      id                         = "8cc1d386-1c7a-11ec-9621-0242ac130002"
+      type                       = "User"
+      user_consent_description   = "Allow the application to access example on your behalf."
+      user_consent_display_name  = "Access django-azure-b2c-auth"
+      value                      = "user_impersonation"
+    }
+  }
+
+  app_role {
+    allowed_member_types = ["User", "Application"]
+    description          = "Admins can manage roles and perform all task actions"
+    display_name         = "Admin"
+    enabled              = true
+    id                   = "f4a11d7c-1c7a-11ec-9621-0242ac130002"
+    value                = "admin"
+  }
+
+  required_resource_access {
+    resource_app_id = "00000003-0000-0000-c000-000000000000" # Microsoft Graph
+
+    resource_access {
+      id   = "df021288-bdef-4463-88db-98f22de89214" # User.Read.All
+      type = "Role"
+    }
+  }
+
+  web {
+    homepage_url  = "http://localhost:8000"
+    logout_url    = "https://localhost:8000/logout"
+    redirect_uris = ["http://localhost:8000/api/v1/response-oidc"]
+
+    implicit_grant {
+      access_token_issuance_enabled = true
+      id_token_issuance_enabled     = false
+    }
+  }
+}
+
+resource "azuread_application_password" "django_azure_b2c_auth" {
+  display_name          = format("%s-%s",azuread_application.django_azure_b2c_auth.display_name,"client-secret")
+  application_object_id = azuread_application.django_azure_b2c_auth.object_id
+}
